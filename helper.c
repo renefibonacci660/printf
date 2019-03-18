a28f71d88a5002203d81ecd9bf48f85f9b2e94f2
renefibonacci660 <660@holbertonschool.com>
2019-03-18 17:06:07 +0000

    Passes betty checker

---
+++ helper.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * get_arg_type - a function that gets function pointer upon argument type
+ * @s: char s after % sign
+ *
+ * Return: return a pointer to next argument as string
+ */
+
+char *(*get_arg_type(char s))(va_list)
+{
+	int i;
+
+	Arg_Types arg_types[] = {
+		{"c", found_char},
+		{"s", found_string},
+		{"%", found_percent},
+		{"d", found_int},
+		{"i", found_int},
+		{"u", found_unsigned},
+		{"o", found_octal},
+		{"x", found_hex},
+		{NULL, NULL}
+	};
+	for (i = 0; arg_types[i].car; i++)
+	{
+		if (s == *arg_types[i].car)
+			return (arg_types[i].fn);
+	}
+	return (NULL);
+}
